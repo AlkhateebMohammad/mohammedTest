97f9385994e66768a093945d931eded3794af094
AlkhateebMohammad <mohammed.m.alkhateeb@gmail.com>
2024-10-06 17:50:26 +0300

    added

--- tests/register.spec.ts
+++ tests/register.spec.ts
@@ -20,7 +20,7 @@ test.describe("all testCases login",()=>{
         await page.waitForTimeout(4000)
 
      const expected=page.locator("#input-error")
-     await expect(expected).not.toBeVisible()
+     await expect(expected).toBeVisible()
 
 })
 test("forgot password", async ({page})=>{
