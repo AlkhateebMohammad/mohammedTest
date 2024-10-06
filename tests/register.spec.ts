ab46eb3d55ccbe400cb8b91fd6ec331396896044
AlkhateebMohammad <mohammed.m.alkhateeb@gmail.com>
2024-10-06 16:46:27 +0300

    📦 NEW: add test

--- tests/register.spec.ts
+++ tests/register.spec.ts
@@ -1,20 +1,38 @@
 import {test,expect} from "@playwright/test"
 
-
-
-test("should be able to login",async({page})=>{
-    
+test.describe("all testCases login",()=>{
+    test.beforeEach("setup",async({page})=>{
         await page.goto("/login")
         await page.locator('span.submit-login-text').click();
         await page.locator("#username").fill("admin")
         await page.locator("#password").fill("Emma@20192")
         await page.locator("#kc-login").click();
+
+    })
+    test("should be able to login",async({page})=>{
+    
+        
         await page.waitForTimeout(4000)
 
      const expected=page.locator("#input-error")
      await expect(expected).toBeVisible()
 
-   
+})
+test("forgot password", async ({page})=>{
+    const forgotPasswordLink = await page.locator('//a[contains(text(), "Forgot Password?")]');
   
+    // تنفيذ أي إجراء على الرابط
+    await forgotPasswordLink.click();
+    await page.locator("#username").fill("mohammed199761@yahoo.com")
+    const submitButton = await page.locator('input.pf-c-button.pf-m-primary.pf-m-block.btn-lg');
+
+  // تنفيذ إجراء للنقر على الزر
+  await submitButton.click();
+
+    await page.waitForTimeout(3000)
+    const expected=page.locator("#header-login")
+    await expect(expected).toBeVisible()
 
 })
+})
+
